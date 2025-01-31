5f28a3d26e1199595a59b02a4630029b89639aac
rafaelmarzoratti <rafaelmarzoratti02@gmail.com>
2025-01-31 10:06:10 -0300

    Add project files.

---
+++ Biblioteca/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers;
+
+[Route("api/books")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
