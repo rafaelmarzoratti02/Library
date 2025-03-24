35416f9fbafdbc22c012dfe4445fc53ed0e14169
rafaelmarzoratti <rafaelmarzoratti02@gmail.com>
2025-03-24 16:15:41 -0300

    Adicionando autenticação e autorização.

---
+++ Library.Application/Commands/UserCommands/InsertUser/InsertUserCommand.cs
@@ -0,0 +1,19 @@
+﻿using Library.Application.Models;
+using Library.Core.Entities;
+using MediatR;
+
+namespace Library.Application.Commands.UserCommands.InsertUser;
+
+public class InsertUserCommand : IRequest<ResultViewModel<int>>
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime Birthdate { get; set; } 
+    public string Password { get; set; }
+    public string Role { get; set; }
+
+
+
+    public User ToEntity(string password)
+        => new(Name, Email, Birthdate,password, Role);
+}
---
+++ Library.Application/Commands/UserCommands/LoginUser/LoginUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using Azure;
+using Library.Application.Models;
+using MediatR;
+
+namespace Library.Application.Commands.UserCommands.LoginUser;
+
+public class LoginUserCommand : IRequest<ResultViewModel<LoginUserViewModel>>
+{
+    public string Email { get; set; }
+    public string Senha { get; set; }
+
+}
