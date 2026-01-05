a9a8a885736249382c74b65c26cf44ed6410dbe0
rafaelmarzoratti <rafaelmarzoratti02@gmail.com>
2026-01-05 17:14:25 -0300

    mudando nome no swagger

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -20,7 +20,7 @@ builder.Services
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Library.API", Version = "v1" });
 
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
